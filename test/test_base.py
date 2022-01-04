bda7fc239ce82b2557d111a13cdbf96eb80fba36
César Ferradas <cesar@herondata.io>
2022-01-04 05:13:23 -0500

    add to_dict() convenience function (#11)
    * add to_dict instance method for convenience
    
    * enforce utc in utils function

--- test/test_base.py
+++ test/test_base.py
@@ -7,3 +7,9 @@ class TestRepr(unittest.TestCase):
     def test_repr(self):
         b = BaseResource(heron_id="res_1")
         self.assertEqual(str(b), "<BaseResource: res_1>")
+
+
+class TestToDict(unittest.TestCase):
+    def test_to_dict(self):
+        b = BaseResource(heron_id="res_1")
+        self.assertEqual(b.to_dict(), {"heron_id": "res_1"})
